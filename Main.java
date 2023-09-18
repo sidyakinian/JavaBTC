16498a83f3150b951eb3c165dfda71a82df311c7
Yan Sidyakin <sidyakinian@gmail.com>
2023-09-18 15:43:51 -0400

    Refactor message

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello JavaBTC!");
-
         String message = "Hello JavaBTC!";
+        System.out.println(message);
+
         byte[] messageBytes = message.getBytes();
         byte[] hash = Hasher.hashSHA256(messageBytes);
         System.out.println("Hash: " + Utils.byteArrayToHex(hash));
