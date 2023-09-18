6adc9cca8bcfd58df7580db70a4587f8192cd559
Yan Sidyakin <sidyakinian@gmail.com>
2023-09-18 15:39:48 -0400

    Add sha256 hashing function

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello JavaBTC!");
+
+        String message = "Hello JavaBTC!";
+        byte[] messageBytes = message.getBytes();
+        byte[] hash = Hasher.hashSHA256(messageBytes);
+        System.out.println("Hash: " + Utils.byteArrayToHex(hash));
     }
 }
---
+++ Utils.java
@@ -0,0 +1,12 @@
+import java.util.Formatter;
+
+public class Utils {
+    public static String byteArrayToHex(final byte[] hash) {
+        try (Formatter formatter = new Formatter()) {
+            for (byte b : hash) {
+                formatter.format("%02x", b);
+            }
+            return formatter.toString();
+        }
+    }
+}
