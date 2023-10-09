efb5e58d72545e312635a4ac0cf0372a8668445d
Yan Sidyakin <sidyakinian@gmail.com>
2023-10-09 14:24:42 -0700

    Add point ops and extended euclidian algorithm

---
+++ src/main/EuclidianResult.java
@@ -0,0 +1,5 @@
+package src.main;
+
+import java.math.BigInteger;
+
+public record EuclidianResult(BigInteger r, BigInteger s, BigInteger t) {}
--- src/main/Utils.java
+++ src/main/Utils.java
@@ -1,6 +1,7 @@
 package src.main;
 import java.math.BigInteger;
 import java.util.Formatter;
+import src.main.EuclidianResult;
 
 public class Utils {
     public static String byteArrayToHex(final byte[] hash) {
@@ -14,5 +15,29 @@ public class Utils {
 
     public static boolean lessThan(BigInteger a, BigInteger b) {
         return a.compareTo(b) < 0;
+    }
+
+    public static EuclidianResult extendedEuclideanAlgorithm(BigInteger a, BigInteger b) {
+        BigInteger oldR = a, r = b;
+        BigInteger oldS = BigInteger.ONE, s = BigInteger.ZERO;
+        BigInteger oldT = BigInteger.ZERO, t = BigInteger.ONE;
+
+        while (!r.equals(BigInteger.ZERO)) {
+            BigInteger quotient = oldR.divide(r);
+
+            BigInteger tempR = r;
+            r = oldR.subtract(quotient.multiply(r));
+            oldR = tempR;
+
+            BigInteger tempS = s;
+            s = oldS.subtract(quotient.multiply(s));
+            oldS = tempS;
+
+            BigInteger tempT = t;
+            t = oldT.subtract(quotient.multiply(t));
+            oldT = tempT;
+        }
+
+        return new EuclidianResult(oldR, oldS, oldT);
     }
 }
