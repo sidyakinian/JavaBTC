02903d56104de277e92fed84f44a551bc3d4dca6
Yan Sidyakin <sidyakinian@gmail.com>
2023-10-09 13:59:36 -0700

    Redefine curve and point with big integers

--- src/main/Curve.java
+++ src/main/Curve.java
@@ -1,9 +1,17 @@
 package src.main;
 
-public record Curve(int p, int a, int b) {
-    public static final Curve bitcoin_curve = new Curve(
-        Integer.parseInt("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F", 16),
-        Integer.parseInt("0x0000000000000000000000000000000000000000000000000000000000000000", 16),
-        Integer.parseInt("0x0000000000000000000000000000000000000000000000000000000000000007", 16)
+import java.math.BigInteger;
+
+public record Curve(BigInteger p, BigInteger a, BigInteger b) {
+    public static final Curve bitcoinCurve = new Curve(
+        new BigInteger("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F", 16),
+        new BigInteger("0x0000000000000000000000000000000000000000000000000000000000000000", 16),
+        new BigInteger("0x0000000000000000000000000000000000000000000000000000000000000007", 16)
+    );
+
+    public static final Curve zeroCurve = new Curve(
+        new BigInteger("0"),
+        new BigInteger("0"),
+        new BigInteger("0")
     );
 }
